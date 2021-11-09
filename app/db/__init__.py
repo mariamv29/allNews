f0673121d605f0f38dd307f4af38b0de602d41bf
Maria Mireles <mireles335@gmail.com>
2021-11-08 21:06:15 -0600

    adding User model

---
+++ app/db/__init__.py
@@ -0,0 +1,14 @@
+from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# connect to database using env variable
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# generates temp connections to the database
+Session = sessionmaker(bind=engine)
+# maps the models to real mySQL tables
+Base = declarative_base()
