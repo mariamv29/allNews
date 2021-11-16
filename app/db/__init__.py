65d0c5c37c4124b8fa7fe7cf79e10870c0612bff
Maria Mireles <mireles335@gmail.com>
2021-11-15 18:19:56 -0600

    applying context in flask

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
+
 
 load_dotenv()
 
@@ -12,3 +14,14 @@ engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0
 Session = sessionmaker(bind=engine)
 # maps the models to real mySQL tables
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+
+def get_db():
+    if 'db' not in g:
+        # sore db connection in app context 
+        g.db = Session()
+        
+    return g.db
